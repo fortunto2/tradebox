8a70ab57c4c1816aa1f38a6918605fd33bd41320
Sadzuke <tuktarov.timur2016@yandex.ru>
2024-07-14 22:11:23 +0300

    update

--- server_start.bat
+++ server_start.bat
@@ -1,7 +1,7 @@
 @echo off
 rem Запуск скрипта Python через Poetry
 @REM poetry run python main.py
-poetry run uvicorn main:app --host 45.32.253.11 --port 8000 --log-level info --reload
+poetry run uvicorn main:app --host 45.32.253.11 --port 80 --log-level info --reload
 
 rem Пауза перед закрытием окна командной строки (необязательно)
 pause
