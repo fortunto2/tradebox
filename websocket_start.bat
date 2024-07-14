f929098b2e5aefb8b02d600a9e66ae23bc701f0f
Sadzuke <tuktarov.timur2016@yandex.ru>
2024-07-14 21:50:08 +0300

    update

--- websocket_start.bat
+++ websocket_start.bat
@@ -1,6 +1,6 @@
 @echo off
 rem Запуск скрипта Python через Poetry
-poetry run python trade/ws_monitor.py
+poetry run python ws_monitor.py
 
 rem Пауза перед закрытием окна командной строки (необязательно)
 pause
