f4dc6e3ea4c863be224850abdb1fe5c3cac73611
Sadzuke <tuktarov.timur2016@yandex.ru>
2024-07-17 23:34:38 +0300

    add postgres

--- ws_FLOKI.bat
+++ ws_FLOKI.bat
@@ -1,6 +1,6 @@
 @echo off
 rem Запуск скрипта Python через Poetry
-poetry run python ws_monitor.py --symbol=FLOKIUSDT
+poetry run python ws_monitor.py --symbol=1000FLOKIUSDT
 
 rem Пауза перед закрытием окна командной строки (необязательно)
 pause
