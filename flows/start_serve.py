85f6714050501577531e93bfe6a563f398e5c07d
Sadzuke <tuktarov.timur2016@yandex.ru>
2024-07-21 17:22:36 +0300

    update

--- flows/start_serve.py
+++ flows/start_serve.py
@@ -10,5 +10,5 @@ from flows.order_filled_flow import order_filled_flow
 
 filled_depl = order_filled_flow.to_deployment(name='order_filled_flow')
 open_long_depl = open_long_position.to_deployment(name='open_long_position')
-cancel_depl = order_cancel_flow(name='order_cancel_flow')
+cancel_depl = order_cancel_flow.to_deployment(name='order_cancel_flow')
 serve(filled_depl, open_long_depl, cancel_depl)
