008adfa62a53d392cb95793984b9b9e21a66b270
Sadzuke <tuktarov.timur2016@yandex.ru>
2024-07-21 16:28:29 +0300

    update

--- core/models/binance_position.py
+++ core/models/binance_position.py
@@ -1,20 +1,20 @@
-import enum
-from datetime import datetime, timedelta
-from decimal import Decimal
-
-from sqlmodel import SQLModel, Field
-from core.models.orders import OrderPositionSide
-
-
-class BinancePosition(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
-    symbol: Field(str, index=True)
-    position_side: OrderPositionSide
-    position_qty: Decimal
-    entry_price: Decimal
-    pnl: Decimal = Field(default=0)
-
-    created_at: datetime = Field(
-        default_factory=datetime.now,
-        nullable=False,
-    )
+# import enum
+# from datetime import datetime, timedelta
+# from decimal import Decimal
+#
+# from sqlmodel import SQLModel, Field
+# from core.models.orders import OrderPositionSide
+#
+#
+# class BinancePosition(SQLModel, table=True):
+#     id: int = Field(default=None, primary_key=True)
+#     symbol: Field(str, index=True)
+#     position_side: OrderPositionSide
+#     position_qty: Decimal
+#     entry_price: Decimal
+#     pnl: Decimal = Field(default=0)
+#
+#     created_at: datetime = Field(
+#         default_factory=datetime.now,
+#         nullable=False,
+#     )
